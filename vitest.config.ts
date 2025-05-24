f4e4aa4485a9ead050bcf0849d95891c7b67de54
Shukkai <98263906+Shukkai@users.noreply.github.com>
2025-05-24 16:58:25 +0000

    done

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
-      reporter: ['text', 'json', 'html']
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: './coverage',
+      include: ['src'],
     },
     exclude: ['node_modules', 'dist', 'frontend/tests']
   }
