853717f4b8dc756ba23e1c05f1faced3d4ffddfe
Shukkai <98263906+Shukkai@users.noreply.github.com>
2025-05-24 04:54:48 +0000

    ci

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    pool: 'forks'
-  }
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
+    exclude: ['node_modules', 'dist'],
+  },
 })
